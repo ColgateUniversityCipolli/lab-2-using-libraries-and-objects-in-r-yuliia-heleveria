5a2ed19208a1efe7d20ffe7e1811d5da0b98b3de
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-01-28 15:16:51 -0500

    Finished 3.3.b

--- Lab2.R
+++ Lab2.R
@@ -10,6 +10,10 @@ num.of.slashes <- str_count(all.subdirectories, pattern = '/')
 all.album.subdirectories <- subset(all.subdirectories, num.of.slashes == 2) 
 #print(all.album.subdirectories)
 
+#Part 3.3
+#creating an empty vector
+code.to.process <- rep(x=NA, times= length(wav.files)) #EDIT
+
 #Part 3
 for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
   #Part 3.1
@@ -25,13 +29,16 @@ for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
   wav.files <- subset(all.files, count.wav > 0)
   #print(wav.files)
   
-  #Part 3.3
-  #creating an empty vector
-  code.to.process <- rep(x=NA, times= length(wav.files))
-  
   #loop through each track
   for (j in 1:length(wav.files)){
-    #part 3.3.a
-    track.location = paste(all.album.subdirectories[i], wav.files[j])
+    #part 3.3.a  - create track file location
+    track.location = paste(all.album.subdirectories[i], wav.files[j], sep = '/')
+    print(track.location)
+    #part 3.3.b - current track's file name
+    remove.wav = str_sub(wav.files[j], start = 1, end = -5) #remove .wav
+    print(remove.wav)
+    split.string <- str_split(remove.wav, '-', simplify = T)
+    track.name = split.string[3]
+    print(track.name)
   }
 }
