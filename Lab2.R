aed3c11cb325a2ffe11ef6661520793391387482
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-01-28 15:42:08 -0500

    Finished the whole 3.3 in task 1

--- Lab2.R
+++ Lab2.R
@@ -12,7 +12,7 @@ all.album.subdirectories <- subset(all.subdirectories, num.of.slashes == 2)
 
 #Part 3.3
 #creating an empty vector
-code.to.process <- rep(x=NA, times= length(wav.files)) #EDIT
+code.to.process <- c() #create an empty vector in the beginning
 
 #Part 3
 for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
@@ -31,15 +31,18 @@ for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
   
   #loop through each track
   for (j in 1:length(wav.files)){
+    
     #part 3.3.a  - create track file location
     track.location = paste(all.album.subdirectories[i], wav.files[j], sep = '/')
     #print(track.location)
+    
     #part 3.3.b - current track's file name
     remove.wav = str_sub(wav.files[j], start = 1, end = -5) #remove .wav
     #print(remove.wav)
     split.string.track <- str_split(remove.wav, '-', simplify = T)
     track.name = split.string.track[3]
     #print(track.name)
+    
     #part 3.3.c - create desired output file
     artist.name <- split.string.track[2] #get artist name
     split.albumn.subdir <- str_split(all.album.subdirectories[i], '/', simplify =T)
@@ -47,7 +50,13 @@ for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
     #file without .json
     output.file <- paste(artist.name, albumn.name, track.name, sep = '-')
     final.file <- paste(output.file, "json", sep = '.') #add .json to the end
-    print(final.file)
+    #print(final.file)
     
+    #part 3.3.d
+    #create command line prompt
+    command.line = paste("streaming_extractor_music.exe", wav.files[j], final.file, sep = ' ')
+    #print(command.line)
+    #append command line to code.to.process vector
+    code.to.process <- c(code.to.process, command.line)
   }
 }
