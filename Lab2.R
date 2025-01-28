58a38bd8788f931b462d078488038f867b96a068
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-01-28 15:32:21 -0500

    Finished 3.3.c

--- Lab2.R
+++ Lab2.R
@@ -33,12 +33,21 @@ for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
   for (j in 1:length(wav.files)){
     #part 3.3.a  - create track file location
     track.location = paste(all.album.subdirectories[i], wav.files[j], sep = '/')
-    print(track.location)
+    #print(track.location)
     #part 3.3.b - current track's file name
     remove.wav = str_sub(wav.files[j], start = 1, end = -5) #remove .wav
-    print(remove.wav)
-    split.string <- str_split(remove.wav, '-', simplify = T)
-    track.name = split.string[3]
-    print(track.name)
+    #print(remove.wav)
+    split.string.track <- str_split(remove.wav, '-', simplify = T)
+    track.name = split.string.track[3]
+    #print(track.name)
+    #part 3.3.c - create desired output file
+    artist.name <- split.string.track[2] #get artist name
+    split.albumn.subdir <- str_split(all.album.subdirectories[i], '/', simplify =T)
+    albumn.name <- split.albumn.subdir[3] #get album name
+    #file without .json
+    output.file <- paste(artist.name, albumn.name, track.name, sep = '-')
+    final.file <- paste(output.file, "json", sep = '.') #add .json to the end
+    print(final.file)
+    
   }
 }
