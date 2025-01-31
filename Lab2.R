5e05d7fa5bfd88175b70000b9a38fb2561ce8ab9
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-01-31 15:28:21 -0500

    Cleaned up R code

--- Lab2.R
+++ Lab2.R
@@ -2,15 +2,12 @@
 
 #Part 1
 all.subdirectories <- list.dirs("MUSIC") #list all sub directories
-#print(all.subdirectories)
 
 #Part 2
 #count all forward slashes
 num.of.slashes <- str_count(all.subdirectories, pattern = '/') 
-#print(num.of.slashes)
 #subset album sub directories
 all.album.subdirectories <- subset(all.subdirectories, num.of.slashes == 2) 
-#print(all.album.subdirectories)
 
 #Part 3.3
 #creating an empty vector
@@ -21,29 +18,23 @@ for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
   #Part 3.1
   #list files in current album
   all.files <- list.files(all.album.subdirectories[i]) 
-  #print(all.files)
   
   #Part 3.2
   #count number of .wav in each file
   count.wav <- str_count(all.files, pattern = '.wav')
-  #print(count.wav)
   #subset all .wav files from album's all files
   wav.files <- subset(all.files, count.wav > 0)
-  #print(wav.files)
   
   #loop through each track
   for (j in 1:length(wav.files)){
     
     #part 3.3.a  - create track file location
     track.location = paste(all.album.subdirectories[i], wav.files[j], sep = '/')
-    #print(track.location)
     
     #part 3.3.b - current track's file name
     remove.wav = str_sub(wav.files[j], start = 1, end = -5) #remove .wav
-    #print(remove.wav)
     split.string.track <- str_split(remove.wav, '-', simplify = T)
     track.name = split.string.track[3]
-    #print(track.name)
     
     #part 3.3.c - create desired output file
     artist.name <- split.string.track[2] #get artist name
@@ -52,12 +43,10 @@ for (i in 1:length(all.album.subdirectories)){ #for each album sub directory
     #file without .json
     output.file <- paste(artist.name, albumn.name, track.name, sep = '-')
     final.file <- paste(output.file, "json", sep = '.') #add .json to the end
-    #print(final.file)
     
     #part 3.3.d
     #create command line prompt
     command.line = paste("streaming_extractor_music.exe", wav.files[j], final.file, sep = ' ')
-    #print(command.line)
     #append command line to code.to.process vector
     code.to.process <- c(code.to.process, command.line)
   }
